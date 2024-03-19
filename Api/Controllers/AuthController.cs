6349f2022176015ecf42ecf9d543164b39972c1b
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-03-19 18:22:57 +0100

    Creacion de la clase Auth para endpoints de login y register

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using ActuArte.Models;
+using ActuArte.Business;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace ActuArte.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class AuthController : ControllerBase
+    {
+        
+    }
+}
